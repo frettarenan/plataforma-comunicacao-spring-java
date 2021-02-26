f79ceb2924de8166662495df65d45322b9b0e531
renanfretta <renanfretta@vhintranet.com.br>
2021-02-26 20:08:28 -0300

    Criação da estrutura básica do projeto

---
+++ pc-plataforma-comunicacao/src/main/java/br/com/renanfretta/pc/plataformacomunicacao/PcPlataformaComunicacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.renanfretta.pc.plataformacomunicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcPlataformaComunicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcPlataformaComunicacaoApplication.class, args);
+	}
+
+}
