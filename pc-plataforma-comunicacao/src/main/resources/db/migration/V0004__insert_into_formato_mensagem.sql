15c5af1038e5ac9b57f98c2fb6467c04efc3739b
renanfretta <renanfretta@vhintranet.com.br>
2021-02-26 22:08:08 -0300

    Script para inserir dados cadastrais do formato de mensagem

---
+++ pc-plataforma-comunicacao/src/main/resources/db/migration/V0004__insert_into_formato_mensagem.sql
@@ -0,0 +1,4 @@
+INSERT INTO `formato_mensagem`(nome) VALUES('E-mail');
+INSERT INTO `formato_mensagem`(nome) VALUES('SMS');
+INSERT INTO `formato_mensagem`(nome) VALUES('Push');
+INSERT INTO `formato_mensagem`(nome) VALUES('WhatsApp');
